58fa217ef97d1276b170189907e9db4b8d01d0a6
Xiaotian Duan <xduan7@gmail.com>
2021-08-02 14:00:10 -0500

    implement basic megatron bert run

---
+++ third-party/scripts/paths.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+CURR_DIR_PATH="$(realpath "$(dirname "$0")")"
+PROJECT_DIR_PATH="$(realpath "${CURR_DIR_PATH}/../../")"
+CONDA_PATH="${HOME}/software/anaconda3/etc/profile.d/conda.sh"
+
+########################################################################
+# Data directories
+########################################################################
+DATA_DIR_PATH="${PROJECT_DIR_PATH}/data"
+
+RAW_DATA_DIR_PATH="${DATA_DIR_PATH}/raw"
+INTERIM_DATA_DIR_PATH="${DATA_DIR_PATH}/interim"
+PROCESSED_DATA_DIR_PATH="${DATA_DIR_PATH}/processed"
+
+# English Wikipedia paths
+ENWIKI_VOCAB_DIR_PATH="${RAW_DATA_DIR_PATH}/enwiki_vocab"
+ENWIKI_TEXT_DIR_PATH="${PROCESSED_DATA_DIR_PATH}/enwiki_text"
+
+BERT_CASED_ENWIKI_VOCAB_PATH="${ENWIKI_VOCAB_DIR_PATH}/bert-large-cased-vocab.txt"
+BERT_UNCASED_ENWIKI_VOCAB_PATH="${ENWIKI_VOCAB_DIR_PATH}/bert-large-uncased-vocab.txt"
+GPT2_ENWIKI_VOCAB_PATH="${ENWIKI_VOCAB_DIR_PATH}/gpt2-vocab.json"
+GPT2_ENWIKI_MERGES_PATH="${ENWIKI_VOCAB_DIR_PATH}/gpt2-merges.txt"
+T5_CASED_ENWIKI_VOCAB_PATH="${ENWIKI_VOCAB_DIR_PATH}/t5-cased-vocab.txt"
+T5_UNCASED_ENWIKI_VOCAB_PATH="${ENWIKI_VOCAB_DIR_PATH}/t5-uncased-vocab.txt"
+
+BERT_CASED_ENWIKI_TEXT_DIR_PATH="${ENWIKI_TEXT_DIR_PATH}/bert_cased"
+BERT_UNCASED_ENWIKI_TEXT_DIR_PATH="${ENWIKI_TEXT_DIR_PATH}/bert_uncased"
+GPT2_ENWIKI_TEXT_DIR_PATH="${ENWIKI_TEXT_DIR_PATH}/gpt2"
+T5_CASED_ENWIKI_TEXT_DIR_PATH="${ENWIKI_TEXT_DIR_PATH}/t5_cased"
+T5_UNCASED_ENWIKI_TEXT_DIR_PATH="${ENWIKI_TEXT_DIR_PATH}/t5_uncased"
+
+########################################################################
+# Third-party directories
+########################################################################
+THIRD_PARTY_DIR_PATH="$(dirname "${CURR_DIR_PATH}")"
+
+FAST_MOE_DIR_PATH="${THIRD_PARTY_DIR_PATH}/fast-moe"
+MEGATRON_DIR_PATH="${THIRD_PARTY_DIR_PATH}/megatron-lm"
+
+MEGATRON_CHECKPOINT_DIR_PATH="${MEGATRON_DIR_PATH}/checkpoints"
